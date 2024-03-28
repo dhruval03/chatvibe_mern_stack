d3ed1403e6c8cee3d8e35de4a3cc98a22dff266e
Dhruval Maniyar <maniyardhruval1290@gmail.com>
2024-03-28 22:47:16 +0530

    f

--- server/index.js
+++ server/index.js
@@ -30,7 +30,7 @@ const server = app.listen(process.env.PORT, () =>
 );
 const io = socket(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "http://localhost:5000",
     credentials: true,
   },
 });
